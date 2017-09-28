18b14296a54593cf05dc354b8f03fa7a3515ee5f
root <you@example.com>
2017-09-29 00:22:32 +0200

    cm.sh

--- cm.sh
+++ cm.sh
@@ -3,4 +3,4 @@
 r=$(( $RANDOM % 100 ))
 echo $r
 sleep $r
-cd ~/projekt/brunek/ && echo "t" >> app.js && git add * && git commit -m "$(echo $r)" && git push origin master && git checkout master
+cd ~/projekt/parkura/ && echo "t" >> app.js && git add * && git commit -m "$(echo $r)" && git push origin master && git checkout master
